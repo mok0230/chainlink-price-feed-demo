07e78d904e817050d8053ef9f7356d0b414314a6
mok <mok0230@protonmail.com>
2021-12-01 10:33:58 -0800

    add hasPriceIncreased script

---
+++ scripts/hasPriceIncreased.js
@@ -0,0 +1,20 @@
+const EthUsdTracker = require("../artifacts/contracts/EthUsdTracker.sol/EthUsdTracker.json");
+
+async function main() {
+  const [signer] = await ethers.getSigners();
+
+  console.log('EthUsdTracker', EthUsdTracker);
+
+  const contract = await ethers.getContractAt(EthUsdTracker.abi, "0xb6455202577bd699A4d96fFAFA414F8041951857", signer);
+
+  console.log("contract", contract);
+  console.log("contract.hasPriceIncreased()", await contract.hasPriceIncreased());
+  console.log("contract.initialPrice()", await contract.initialPrice());
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+});
