4aad3b86c63c42019bec19b18ef567f330b591e2
mok <mok0230@protonmail.com>
2021-12-01 11:06:46 -0800

    update contractr address reference in hasPriceIncreased script

--- scripts/hasPriceIncreased.js
+++ scripts/hasPriceIncreased.js
@@ -1,11 +1,13 @@
 const EthUsdTracker = require("../artifacts/contracts/EthUsdTracker.sol/EthUsdTracker.json");
 
 async function main() {
+  const CONTRACT_ADDRESS = "0xb7A363Fe1C464F95DC3D135d33C2De74f1F75c26";
+  
   const [signer] = await ethers.getSigners();
 
   console.log('EthUsdTracker', EthUsdTracker);
 
-  const contract = await ethers.getContractAt(EthUsdTracker.abi, "0xb6455202577bd699A4d96fFAFA414F8041951857", signer);
+  const contract = await ethers.getContractAt(EthUsdTracker.abi, CONTRACT_ADDRESS, signer);
 
   console.log("contract", contract);
   console.log("contract.hasPriceIncreased()", await contract.hasPriceIncreased());
