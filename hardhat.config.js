17cd3116df1f33fff74ad7d202c4c6d0d75261f1
mok <mok0230@protonmail.com>
2021-12-01 10:04:44 -0800

    update compiler version in hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,7 +2,7 @@ require('@nomiclabs/hardhat-waffle');
 require('dotenv').config();
 
 module.exports = {
-  solidity: "0.8.0",
+  solidity: "0.8.7",
   networks: {
     kovan: {
       url: `${process.env.ALCHEMY_KOVAN_URL}`,
